e40baf29b7ecff04a5921a1c065120f8f3428cc4
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-12-10 18:02:25 -0300

    Atualiza����o ou adi����o do arquivo: Ex_01.py

---
+++ 01_Programacao full-stack/03_Lógica_de_Programação/01_Teorias_e_Exercicios/Aula 2/Exercicios/Ex_01.py
@@ -0,0 +1,7 @@
+print("Quantos graus está a piscina?")  
+temp = int(input('Digite o valor: '))  
+
+if 25 <= temp <= 60:  
+    print("Você pode entrar.")  
+else:  
+    print("Você não pode entrar.")
