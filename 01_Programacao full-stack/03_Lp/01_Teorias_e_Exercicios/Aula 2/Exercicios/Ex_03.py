e573d54243cab9f61685ea78045ed483f3dc5961
am-arcanjo <amanda.r.arcanjo@gmail.com>
2024-12-10 17:28:56 -0300

    atualizacao Ex_03.py

---
+++ 01_Programacao full-stack/03_Lp/01_Teorias_e_Exercicios/Aula 2/Exercicios/Ex_03.py
@@ -0,0 +1,10 @@
+print("quanto vc tem na conta")  
+conta = float(input('digite o valor: '))  
+
+print("quanto q eh o valor da compra")  
+compra = float(input('digite o valor: '))  
+
+if conta >= compra:  
+    print("vc tem dinheiro suficiente")  
+else:  
+    print("vc não tem dinheiro suficiente")
